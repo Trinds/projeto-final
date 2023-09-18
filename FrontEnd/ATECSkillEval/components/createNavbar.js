3149d95f9f27b339bb5e8111ef19d79585cdd764
JoseCMRibeiro <119082696+JoseCMRibeiro@users.noreply.github.com>
2023-09-18 10:11:05 +0100

    adding navbar

--- FrontEnd/ATECSkillEval/components/createNavbar.js
+++ FrontEnd/ATECSkillEval/components/createNavbar.js
@@ -1,37 +1,43 @@
+export function createNavbar() {
+  const homeLink = '/';
+  const coursesLink = '/courses';
+  const graduatesLink = '/graduates';
+  const classesLink = '/classes';
+  const reportsLink = '/reports';
+  const evaluationsLink = '/evaluations';
+  const logoutLink = '/logout';
 
-export function createNavbar() 
-{
-    const navbar = document.createElement('div');
-    navbar.className = 'navbar';
-    navbar.innerHTML=`
+  const navbar = document.createElement('div');
+  navbar.className = 'navbar';
 
-      <ul>
+  navbar.innerHTML = `
 
-        <li></li>
+  <li></li>
 
-        <ul>
-          <li id="user">User</li>
-          <li id="role">Role</li>
-        </ul>
+  <ul>
+    <li id="user">User</li>
+    <li id="role">Role</li>
+  </ul>
 
-        <li></li>
+  <li></li>
 
-        <ul id="menu">
-          <li><i class="fa fa-home" aria-hidden="true"></i> Início</li>
-          <li><i class="fa fa-institution" aria-hidden="true"></i> Cursos</li>
-          <li><i class="fa fa-graduation-cap" aria-hidden="true"></i> Formandos</li>
-          <li><i class="fa fa-spinner" aria-hidden="true"></i> Turmas</li>
-          <li><i class="fa fa-sticky-note-o" aria-hidden="true"></i>  Relatórios</li>
-          <li><i class="fa fa-reorder" aria-hidden="true"></i> Avaliações</li>
-        </ul>
 
-        <li></li>
-        <li></li>
+    <ul>
+      <li><a href="${homeLink}"><i class="fa fa-home"></i> Início</a></li>
+      <li><a href="${coursesLink}"><i class="fa fa-institution"></i> Cursos</a></li>
+      <li><a href="${graduatesLink}"><i class="fa fa-graduation-cap"></i> Formandos</a></li>
+      <li><a href="${classesLink}"><i class="fa fa-spinner"></i> Turmas</a></li>
+      <li><a href="${reportsLink}"><i class="fa fa-sticky-note-o"></i> Relatórios</a></li>
+      <li><a href="${evaluationsLink}"><i class="fa fa-reorder"></i> Avaliações</a></li>
+    </ul>
 
-        <ul id="sair">
-          <li>SAIR</i>
-        </ul>
-      
-    `;
-    return navbar;
+    <li></li>
+    <li></li>
+    
+    <ul id="sair">
+      <li><a href="${logoutLink}">SAIR</a></li>
+    </ul>
+  `;
+
+  return navbar;
 }
