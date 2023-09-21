f5417df2c6e62913428ab53c390cbe753e5e2420
Trinds <carlos.bem.t0123933@edu.atec.pt>
2023-09-21 23:02:48 +0100

    starting student list

--- FrontEnd/ATECSkillEval/src/studentsPage.js
+++ FrontEnd/ATECSkillEval/src/studentsPage.js
@@ -1,5 +1,6 @@
 import '../reset.css'
 import '../style.css'
+import '../styles/students.css'
 import { createSidebar } from '../components/createSidebar.js'
 import createTopbar from "../components/createTopbar.js";
 
