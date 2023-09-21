aa06b541a57d6fea51d4a7434f9b8b2f4fc3b6ca
Trinds <carlos.bem.t0123933@edu.atec.pt>
2023-09-21 15:54:01 +0100

    minor adjustments

--- FrontEnd/ATECSkillEval/components/createNavbar.js
+++ FrontEnd/ATECSkillEval/components/createNavbar.js
@@ -1,41 +1,57 @@
 export function createNavbar() {
-  const homeLink = '/pages/overviewpage.html';
+  const homeLink = '/pages/overviewPage.html';
   const coursesLink = '/pages/courses/';
-  const graduatesLink ='/pages/students/';
-  const classesLink = '/pages/classrooms/';
+  const studentsLink ='/pages/students/';
+  const classroomsLink = '/pages/classrooms/';
   const reportsLink = '/pages/reports/';
   const evaluationsLink = '/pages/evaluations/';
   const logoutLink = '/logout';
+
+  const app = document.querySelector('body');
 
   const navbar = document.createElement('aside');
   navbar.className = 'navbar';
 
   navbar.innerHTML = `
 
-
-
   <ul>
     <li id="user">User</li>
     <li id="role">Role</li>
   </ul>
 
-
-
     <ul id= "menu">
-      <li><a href="${homeLink}"><i class="fa fa-home"></i> Início</a></li>
-      <li><a href="${coursesLink}"><i class="fa-regular fa-bookmark"></i> Cursos</a></li>
-      <li><a href="${graduatesLink}"><i class="fa-solid fa-user-graduate"></i> Formandos</a></li>
-      <li><a href="${classesLink}"><i class="fa-solid fa-users"></i> Turmas</a></li>
-      <li><a href="${reportsLink}"><i class="fa fa-flag"></i> Relatórios</a></li>
-      <li><a href="${evaluationsLink}"><i class="fa-solid fa-check"></i> Avaliações</a></li>
+      <li id="toHome"><div class="icon"><i class="fa fa-home"></i></div>Início</li>
+      <li id="toCourses"><div class="icon"><i class="fa-regular fa-bookmark"></i></div>Cursos</li>
+      <li id="toStudents"><div class="icon"><i class="fa-solid fa-user-graduate"></i></div>Formandos</li>
+      <li id="toClassrooms"><div class="icon"><i class="fa-solid fa-users"></i></div>Turmas</li>
+      <li id="toReports"><div class="icon"><i class="fa fa-flag"></i></div>Relatórios</li>
+      <li id="toEvaluations"><div class="icon"><i class="fa-solid fa-check"></i></div>Avaliações</li>
     </ul>
 
-
-    
     <ul id="sair">
       <li><a href="${logoutLink}">SAIR</a></li>
     </ul>
   `;
 
-  return navbar;
+  app.prepend(navbar)
+
+  document.querySelector('#toHome').addEventListener('click',()=>{
+    location.href=homeLink
+  })
+  document.querySelector('#toCourses').addEventListener('click',()=>{
+    location.href=coursesLink
+  })
+  document.querySelector('#toStudents').addEventListener('click',()=>{
+    location.href=studentsLink
+  })
+  document.querySelector('#toClassrooms').addEventListener('click',()=>{
+    location.href=classroomsLink
+  })
+  document.querySelector('#toReports').addEventListener('click',()=>{
+    location.href=reportsLink
+  })
+  document.querySelector('#toEvaluations').addEventListener('click',()=>{
+    location.href=evaluationsLink
+  })
+
 }
--- FrontEnd/ATECSkillEval/main.js
+++ FrontEnd/ATECSkillEval/main.js
@@ -3,5 +3,4 @@ import './style.css'
 import './styles/navbar.css'
 import { createNavbar } from './components/createNavbar';
 
-const app = document.querySelector('body');
-app.prepend(createNavbar())
+createNavbar()
