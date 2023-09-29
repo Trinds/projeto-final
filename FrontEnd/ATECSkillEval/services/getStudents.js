c811987d2c2c724ed9d70c50e86ade5cb2b70d83
JoseCMRibeiro <119082696+JoseCMRibeiro@users.noreply.github.com>
2023-09-29 08:44:47 +0100

    aval form

--- FrontEnd/ATECSkillEval/services/getStudents.js
+++ FrontEnd/ATECSkillEval/services/getStudents.js
@@ -1,4 +1,11 @@
-async function populateDropdown() 
+window.addEventListener("load", populateClass);
+window.addEventListener("load", populateStudents);
+window.addEventListener("load", populateTeste);
+
+
+
+
+async function populateStudents() 
 {
     const apiUrl = "http://127.0.0.1:8000/api/students/";
 
@@ -6,6 +13,14 @@ async function populateDropdown()
     {
         const response = await fetch(apiUrl);
         const data = await response.json();
+
+        data.sort((a, b) => {
+            const nameA = a.name.toUpperCase();
+            const nameB = b.name.toUpperCase();
+            if (nameA < nameB) return -1;
+            if (nameA > nameB) return 1;
+            return 0;
+        });
 
         const dropdown = document.getElementById("select_aluno");
 
@@ -23,4 +38,64 @@ async function populateDropdown()
     }
 }
 
-window.addEventListener("load", populateDropdown);
+async function populateClass() 
+{
+    const apiUrl = "http://127.0.0.1:8000/api/classrooms/";
+
+    try 
+    {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+
+        data.sort((a, b) => {
+            const courseNameA = a.course.name.toUpperCase();
+            const courseNameB = b.course.name.toUpperCase();
+            if (courseNameA < courseNameB) return -1;
+            if (courseNameA > courseNameB) return 1;
+            return 0;
+        });
+
+        const dropdown = document.getElementById("select_class");
+
+        data.forEach(classroom => 
+            {
+            const option = document.createElement("option");
+            option.value = classroom.id; 
+            option.textContent = `${classroom.course.name} (${classroom.edition})`; 
+            dropdown.appendChild(option);
+        });
+    } 
+    catch (error) 
+    {
+        console.error("Error fetching data:", error);
+    }
+}
+
+async function populateTeste() 
+{
+    const apiUrl = "http://127.0.0.1:8000/api/test_types/";
+
+    try 
+    {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+
+
+
+        const dropdown = document.getElementById("select_test");
+
+        data.forEach(test_types => 
+            {
+            const option = document.createElement("option");
+            option.value = test_types.id; 
+            option.textContent = test_types.type
+            dropdown.appendChild(option);
+        });
+    } 
+    catch (error) 
+    {
+        console.error("Error fetching data:", error);
+    }
+}
