4f20b930a58d67e83385b8b2791e979d10277b54
Trinds <carlos.bem.t0123933@edu.atec.pt>
2023-10-02 18:04:29 +0100

    add button on topbar

--- FrontEnd/ATECSkillEval/components/createTopbar.js
+++ FrontEnd/ATECSkillEval/components/createTopbar.js
@@ -1,13 +1,14 @@
 import '/styles/topbar.css'
 
+const btnDesc = 'Adicionar "Cena"'
 export default function createTopbar(){
     const topbar = document.createElement('div')
     topbar.classList.add('top-bar')
     topbar.innerHTML=`
-    <i class="fa-solid fa-circle-chevron-left fa-xl"></i>
         <div class="search-input">
             <input type="search" class="custom-icon" placeholder="Pesquisar...">
         </div>
+        <button>${btnDesc}</button>
     `
     document.querySelector('#content').prepend(topbar)
 }
